f37ff9dc4122bbf22a9ae138a01de6c3c11047ed
Andrea Zonca <code@andreazonca.com>
2026-02-04 21:46:03 -0800

    Allow run_example to save PNGs

--- simulation-tools/run_example.C
+++ simulation-tools/run_example.C
@@ -1,7 +1,14 @@
 // Simple runner for panodisplay to view example CORSIKA simulation
 // Usage: root -l run_example.C
+// To save PNGs (headless): root -l -b -q 'run_example.C(0,true,"example")'
 
-void run_example(int eventNumber = 0) {
+#include "TCanvas.h"
+#include "TCollection.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "iostream"
+
+void run_example(int eventNumber = 0, bool savePng = false, const char* outputPrefix = "example") {
     std::cout << "Reading example.root" << std::endl;
     std::cout << "Displaying event " << eventNumber << std::endl;
     gROOT->LoadMacro("panodisplay.C");
@@ -18,8 +25,24 @@ void run_example(int eventNumber = 0) {
 
     readFile(rootFile);
 
-    // Set visibility to true to open display window
-    gROOT->SetBatch(kFALSE);
+    // Use batch mode when saving PNGs (no X11 required)
+    gROOT->SetBatch(savePng ? kTRUE : kFALSE);
 
     panodisplay(eventNumber);
+
+    if (savePng) {
+        TIter next(gROOT->GetListOfCanvases());
+        TCanvas* c = nullptr;
+        while ((c = (TCanvas*)next())) {
+            TString name = c->GetName();
+            name.ReplaceAll(" ", "_");
+            TString file;
+            if (outputPrefix && outputPrefix[0] != '\0') {
+                file.Form("%s_%s.png", outputPrefix, name.Data());
+            } else {
+                file.Form("%s.png", name.Data());
+            }
+            c->SaveAs(file);
+        }
+    }
 }
